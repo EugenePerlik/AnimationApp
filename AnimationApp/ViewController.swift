6b8a79aede68137b874e678f4b6b33345d9324e7
EugenePerlik <imybox24@yandex.ru>
2019-02-12 16:09:29 +0300

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by  Apple24 on 12/02/2019.
+//  Copyright © 2019  Apple24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
